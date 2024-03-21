6570b4ffbe6b3da4452a4bb627d75815d49c99b4
Ivan Meleshin <ivansmel@gmail.com>
2024-03-21 20:41:27 +0400

    FP4 and 5 update

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,13 +4,20 @@
 
 /* Implementation of class "MessageQueue" */
 
-
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait()
     // to wait for and receive new messages and pull them from the queue using move semantics.
     // The received object should then be returned by the receive function.
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _cond.wait(uLock, [this]
+               { return !_queue.empty(); });
+    // remove last vector element from queue
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg; // will not be copied due to return value optimization (RVO) in C++
 }
 
 template <typename T>
@@ -18,8 +25,11 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex>
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::this_thread::sleep_for(std::chrono::milliseconds(2));
+    std::lock_guard<std::mutex> uLock(_mutex);
+    _queue.push_back(msg);
+    _cond.notify_one();
 }
-
 
 /* Implementation of class "TrafficLight" */
 
@@ -34,6 +44,11 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop
     // runs and repeatedly calls the receive function on the message queue.
     // Once it receives TrafficLightPhase::green, the method returns.
+    while(true)
+    {
+        TrafficLightPhase currentPhase = _msgQ_TrLightPh.receive();
+        if (currentPhase==TrafficLightPhase::green) {return;} 
+    }
 }
 
 TrafficLight::TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -73,8 +88,9 @@ void TrafficLight::cycleThroughPhases()
             // toggle
             _currentPhase = (_currentPhase == TrafficLightPhase::green) ? TrafficLightPhase::red : TrafficLightPhase::green;
 
-            // TODO  sends an update method to the message queue using move semantics
-            // _msgQ_TrLightPh.send(std::move(_currentPhase));
+            // TODO  сcheck sends an update method to the message queue using move semantics
+            TrafficLightPhase currentPhase = _currentPhase;
+            _msgQ_TrLightPh.send(std::move(currentPhase));
             start_time = std::chrono::high_resolution_clock::now();
             randomNumber = dist(gen);
         }
